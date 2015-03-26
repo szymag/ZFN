c72b961467e0a2befa612153953e2ea3ceb93e75
Szymon Mieszczak <szymon.mieszczak@gmail.com>
2015-03-26 16:05:26 +0100

    added two new classes: SiatkaPunktow Magnetyzacja

--- siec_odwrotna.py
+++ siec_odwrotna.py
@@ -1,8 +1,37 @@
 __author__ = 'sz'
 import math
+import cmath
 
 import numpy as np
 import scipy.special
+
+
+class SiatkaPunktow(object):
+    def __init__(self, zakres_x, zakres_y, krok):
+        self.zakres_x = zakres_x
+        self.zakres_y = zakres_y
+        self.krok = krok
+
+    def generowanie_punktow(self, liczba):
+        temp = 0
+        lista = []
+        while liczba >= temp:
+            lista.extend([temp])
+            temp += self.krok
+        return lista
+
+    def siatka(self):
+
+        tempx = self.generowanie_punktow(self.zakres_x)
+        tempy = self.generowanie_punktow(self.zakres_y)
+
+        tablica = []
+        for ii in tempx:
+            temp = []
+            for jj in tempy:
+                temp.append([ii, jj, 0])
+            tablica.append(temp)
+        return tablica
 
 
 class WektorySieci(object):
@@ -44,12 +73,6 @@ class WektorySieci(object):
         return [a + b for a, b in zip(self.wektor_b(1), self.wektor_b(2))]
 
 
-# q = WektorySieci(10, 10, 90, 5, 5)
-
-# a = q.lista_wektorow_b1()
-# print(a[0][0])
-
-
 class SiecKwadratowa(WektorySieci):
     d = 6
     a = 10
@@ -61,7 +84,7 @@ class SiecKwadratowa(WektorySieci):
     def __init__(self, dlugosc_a1, dlugosc_a2, kat, zakres_1, zakres_2):
         WektorySieci.__init__(self, dlugosc_a1, dlugosc_a2, kat, zakres_1, zakres_2)
 
-    def wspolczynnik(self, k, liczba1, liczba2):
+    def funkcja(self, k, liczba1, liczba2):
 
         if k == 1:
             wspolczynnik_fouriera = (self.MoA - self.MoB) * self.d ** 2 / self.s * \
@@ -80,7 +103,7 @@ class SiecKwadratowa(WektorySieci):
         for ii in range(len(gx)):
             temp = []
             for jj in range(len(gy)):
-                wspolczynnik_fouriera = self.wspolczynnik(1, gx[ii][0], gy[jj][1])
+                wspolczynnik_fouriera = self.funkcja(1, gx[ii][0], gy[jj][1])
                 temp.append([gx[ii][0], gy[jj][1], wspolczynnik_fouriera])
             wsp.append(temp)
         wsp[int(len(gx) / 2)][int(len(gy) / 2)][2] = self.MoA * (self.d ** 2 / self.s) \
@@ -94,14 +117,52 @@ class SiecKwadratowa(WektorySieci):
         for ii in range(len(gx)):
             temp = []
             for jj in range(len(gy)):
-                wspolczynnik_fouriera = self.wspolczynnik(2, gx[ii][0], gy[jj][1])
+                wspolczynnik_fouriera = self.funkcja(2, gx[ii][0], gy[jj][1])
                 temp.append([gx[ii][0], gy[jj][1], wspolczynnik_fouriera])
             wsp.append(temp)
-        wsp[int(len(gx) / 2)][int(len(gy) / 2)][2] = (self.MoA - self.MoB) * math.pi * self.R ** 2 / self.s + self.MoB
+        wsp[int(len(gx) / 2)][int(len(gy) / 2)][2] = \
+            (self.MoA - self.MoB) * math.pi * self.R ** 2 / self.s + self.MoB
         return wsp
 
 
-q = SiecKwadratowa(10, 10, 90, 5, 5)
-# print(q.rdzen_okragly())
-print("")
-print(q.rdzen_okragly())
+class Magnetyzacja(object):
+    object1 = SiatkaPunktow(5, 5, 0.1)
+    siatka = object1.siatka()
+    object2 = SiecKwadratowa(10, 10, 90, 5, 5)
+    kwadratowa_okragy = object2.rdzen_okragly()
+    kwadratowa_kwadratowy = object2.rdzen_kwadratowy()
+
+    def __init__(self):
+        pass
+
+    def magnetyzacja_w_punkcie(self, parametry, x, y):
+        temp = 0
+        for ii in range(len(parametry)):
+            for jj in range(len(parametry)):
+                temp += (parametry[ii][jj][2]
+                         * (cmath.exp(complex(1j) * (parametry[ii][jj][0] * x + (parametry[ii][jj][1] * y))))).real
+        return temp
+
+    def magnetyzacja(self, parametry):
+        table = self.siatka
+        for ii in range(len(table)):
+            for jj in range(len(table)):
+                table[ii][jj][2] = \
+                    self.magnetyzacja_w_punkcie(parametry, table[ii][jj][0], table[ii][jj][1])
+        return table
+
+    def magnetyzacja_dla_sieci(self, k):
+        if k == 1:
+            return self.magnetyzacja(self.kwadratowa_okragy)
+        elif k == 2:
+            return self.magnetyzacja(self.kwadratowa_kwadratowy)
+        elif k == 3:
+            return None
+        elif k == 4:
+            return None
+        else:
+            return None
+
+
+w = Magnetyzacja()
+print(w.magnetyzacja_dla_sieci(1))
