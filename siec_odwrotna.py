37c32824ca1ba80f8f1c058520509afcddc82289
Szymon Mieszczak <szymon.mieszczak@gmail.com>
2015-03-31 21:02:49 +0200

    pisanie dokumentacji

--- siec_odwrotna.py
+++ siec_odwrotna.py
@@ -102,6 +102,7 @@ class SiecKwadratowa(WektorySieci):
     MoB = 4
     R = 3
 
+
     def __init__(self, dlugosc_a1, dlugosc_a2, kat, zakres_1, zakres_2):
         WektorySieci.__init__(self, dlugosc_a1, dlugosc_a2, kat, zakres_1, zakres_2)
 
