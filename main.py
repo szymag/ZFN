529d4bcc190badb281fed1247e622f17e11be8bf
Szymon Mieszczak <szymon.mieszczak@gmail.com>
2015-07-09 11:57:39 +0200

    problem with 'plot' class

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 __author__ = 'szymag'
 
 from src.Magnetyzacja import Magnetyzacja
-from src.SiecKwadratowa import SiecKwadratowa
+
+from src.SiecTrojkatna import SiecTrojkatna
 from src.SiatkaPunktow import SiatkaPunktow
 from src.Plot import Plot
 
-siatka1 = SiatkaPunktow(5, 5, 0.1)
-siec1 = SiecKwadratowa(10, 10, 5, 5)
-w = Magnetyzacja(siatka1, siec1)
+siatka = SiatkaPunktow(5, 5, 0.07)
+siec = SiecTrojkatna(10, 10, 18, 18)
+w = Magnetyzacja(siatka, siec)
 
-siatka2 = SiatkaPunktow(5, 5, 0.1)
-siec2 = SiecKwadratowa(10, 10, 10, 10)
-ww = Magnetyzacja(siatka1, siec1)
-
-r = Plot(ww.magnetyzacja_dla_sieci('kwadratowy'))
-
+r = Plot(w.magnetyzacja_dla_sieci('okragly'))
 
 # r.wykres_pcolor()
 
-print(r.plot())
+r.plot()
