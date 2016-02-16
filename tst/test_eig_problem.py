0e4041c002455e0a05c0060f23c70fbd9032d184
szymag <szymon.mieszczak@gmail.com>
2016-02-16 10:48:53 +0100

    significantly improvment of efficiency
    (cherry picked from commit a5d20a3817806222d8bf091f4ae641c3fa459003)
    
    # Conflicts:
    #	src/eig_problem/DFT.py
    #	src/eig_problem/FFTfromFile.py
    #	src/eig_problem/MacierzDoZagadnienia.py
    #	src/eig_problem/WektorySieciOdwrotnej.py
    #	src/eig_problem/ZagadnienieWlasne.py
    #	tst/test_eig_problem.py

--- tst/test_eig_problem.py
+++ tst/test_eig_problem.py
@@ -10,7 +10,6 @@ class TestEigProblem(unittest.TestCase):
     q = MacierzDoZagadnienia(9, 'FFT', 'I')
     w = FFTfromFile(9, 'I')
     e = WektorySieciOdwrotnej(30e-9, 30e-9, 9)
-    ee = WektorySieciOdwrotnej(300e-9, 300e-9, 9)
 
     def test_suma_roznica_wektorow(self):
         self.assertEqual(self.q.suma_roznica_wektorow((0, 0), (0, 0), '+'), (0, 0))
@@ -32,3 +31,4 @@ class TestEigProblem(unittest.TestCase):
         self.assertEqual(self.q.norma_wektorow((100, 11.5), (10, -20), '+'), math.sqrt(110 ** 2 + (-8.5) ** 2))
         self.assertEqual(self.q.norma_wektorow((10, 10), (-50, -50), '+'), math.sqrt(40 ** 2 + 40 ** 2))
         self.assertEqual(self.q.norma_wektorow((1000, 0), (0, 0), '+'), 1000)
+
