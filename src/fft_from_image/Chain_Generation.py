c211c18e6d0a4ce793474218b095d595fe4a9f67
szymon <szymon.miszczak@gmail.com>
2016-06-27 10:44:19 +0200

    Thue-Morse sequence was added

--- src/fft_from_image/Chain_Generation.py
+++ src/fft_from_image/Chain_Generation.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 
 class Chain_Generation:
 
-    def __init__(self, num_blocks, repeat):
-        self.num_blocks = num_blocks
+    def __init__(self, fib_num, repeat, tm_num):
+        self.fib_num = fib_num
         self.repeat = repeat
+        self.tm_num = tm_num
 
     def fib_number(self):
-        n = self.num_blocks
+        n = self.fib_num
         i = h = 1
         j = k = 0
         while (n > 0):
@@ -27,7 +28,7 @@ class Chain_Generation:
         seq1 = [1]
         seq2 = [0]
         seq = seq2 + seq1
-        for i in range(self.num_blocks - 2):
+        for i in range(self.fib_num - 2):
             seq = seq2 + seq1
             seq1 = seq2
             seq2 = seq
@@ -37,3 +38,15 @@ class Chain_Generation:
         seq = np.zeros(self.fib_number())
         seq[::2] += 1
         return np.repeat(seq, self.repeat)
+
+    def tm_construct(self, seq):
+        return [(i + 1)%2  for i in seq]
+
+    def thue_morse_seq(self):
+        seq = [0]
+        for i in range(self.tm_num):
+            seq = seq + self.tm_construct(seq)
+        return np.repeat(seq, self.repeat)
+
+q = Chain_Generation(4, 1, 0)
+print(q.thue_morse_seq())
