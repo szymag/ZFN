c7ecd959823dc4a72ae7bcf87fdf09e697ec1e9b
szymon <szymon.miszczak@gmail.com>
2016-11-16 13:11:57 +0100

    first attempt to build unit test, but there are no completed any class.

--- src/start_program.py
+++
@@ -1,3 +0,0 @@
-from src.eig_problem.ZagadnienieWlasne import start
-
-start(101*101)
--- tst/test_eig_problem.py
+++ tst/test_eig_problem.py
@@ -1,34 +1,22 @@
 import math
 import unittest
-
+import  numpy as np
 from src.eig_problem.FFTfromFile import FFTfromFile
 from src.eig_problem.MacierzDoZagadnienia import MacierzDoZagadnienia
 from src.eig_problem.WektorySieciOdwrotnej import WektorySieciOdwrotnej
 
+class MacierzDoZagadnieniaTestCases(unittest.TestCase):
+    parameters = np.genfromtxt('set_of_parameters.dat', dtype=None, delimiter=', ')
+    #tmp = MacierzDoZagadnienia(parameters)
+    print(parameters[0][0].astype(str))
 
-class TestEigProblem(unittest.TestCase):
-    q = MacierzDoZagadnienia(9, 'FFT', 'I')
-    w = FFTfromFile(9, 'I')
-    e = WektorySieciOdwrotnej(30e-9, 30e-9, 9)
+    def build_objects(self):
+        pass
+        #for i in enumerate(parameters)
 
-    def test_suma_roznica_wektorow(self):
-        self.assertEqual(self.q.suma_roznica_wektorow((0, 0), (0, 0), '+'), (0, 0))
-        self.assertEqual(self.q.suma_roznica_wektorow((0, 0), (0, 0), '-'), (0, 0))
-        self.assertEqual(self.q.suma_roznica_wektorow((-3, 3), (0, 0), '+'), (-3, 3))
-        self.assertEqual(self.q.suma_roznica_wektorow((0, 15), (5, 0), '-'), (-5, 15))
-        self.assertEqual(self.q.suma_roznica_wektorow((0, 0), (0, 10), '+'), (0, 10))
-        self.assertEqual(self.q.suma_roznica_wektorow((0, 3), (0, 3), '-'), (0, 0))
-        self.assertEqual(self.q.suma_roznica_wektorow((4.12, 2), (0, 3120), '+'), (4.12, 3122))
-        self.assertEqual(self.q.suma_roznica_wektorow((0, 0), (121340, 0), '+'), (121340, 0))
+    def test_pole_wymiany_II(self):
+        ans_pole_wymiany_II = np.genfromtxt('answers_to_pole_wymiany_II.dat', dtype=None, delimiter=' ')
+        pass
 
-    def test_norma_wektorow(self):
-        self.assertEqual(self.q.norma_wektorow((0, 0), (0, 0), '+'), 0)
-        self.assertEqual(self.q.norma_wektorow((1, 1), (0, 0), '-'), math.sqrt(2))
-        self.assertEqual(self.q.norma_wektorow((1, 1), (0, 0), '+'), math.sqrt(2))
-        self.assertEqual(self.q.norma_wektorow((1, 1), (-1, -1), '-'), math.sqrt(8))
-        self.assertEqual(self.q.norma_wektorow((1, 1), (1, -1), '+'), math.sqrt(4))
-        self.assertEqual(self.q.norma_wektorow((1, 1), (1, -1), '-'), math.sqrt(4))
-        self.assertEqual(self.q.norma_wektorow((100, 11.5), (10, -20), '+'), math.sqrt(110 ** 2 + (-8.5) ** 2))
-        self.assertEqual(self.q.norma_wektorow((10, 10), (-50, -50), '+'), math.sqrt(40 ** 2 + 40 ** 2))
-        self.assertEqual(self.q.norma_wektorow((1000, 0), (0, 0), '+'), 1000)
-
+if __name__ == '__main__':
+    unittest.main()
