953506a3bbf505c48aeee20b34ba65eaaef5a515
szymag <szymon.mieszczak@gmail.com>
2016-12-16 13:30:52 +0100

    create class ReconstructionAngleRotation

--- src/fft_from_image/Reconstruction.py
+++ src/fft_from_image/Reconstruction.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 
 class Reconstruction:
-
     def __init__(self, file):
         self.file = np.loadtxt(file).view(complex)
         self.size = (len(self.file[0]), len(self.file))
@@ -12,14 +11,28 @@ class Reconstruction:
         return np.fft.fft2(self.file)
 
     def plot(self):
-        x, y = np.mgrid[slice(0, self.size[1], 1), slice(0, self.size[0], 1)]
+        y, x = np.mgrid[slice(0, self.size[1], 1), slice(0, self.size[0], 1)]
         z = abs(self.inverse_transform())
-        print(z.shape)
         plt.pcolor(x, y, z, cmap='gray')
         plt.colorbar()
         plt.show()
 
 
+class ReconstructionAngleRotation(Reconstruction):
+    def __init__(self, file, angle):
+        Reconstruction.__init__(self, file)
+        self.angle = angle
+
+    def reciprocal_vector(self):
+        return np.array(np.meshgrid(np.arange(0, self.size[0]), np.arange(0, self.size[1]))).T
+
+    def tmp(self, position):
+        rec_vector = self.reciprocal_vector()
+        print(rec_vector.shape)
+
+    def dft(self):
+        pass
+
 if __name__ == "__main__":
-    q = Reconstruction('F7.txt')
-    q.plot()
+    q = ReconstructionAngleRotation('stripes.txt', 45)
+    q.tmp(1)
